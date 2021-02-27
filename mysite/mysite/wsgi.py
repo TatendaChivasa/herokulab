7a4e57dc8d6bd843f3e1e50feb3d41f977d2ddff
TATENDA CHIVASA <tatenda@TATENDAs-MacBook-Pro.local>
2021-02-27 15:21:57 -0700

    im tired

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
+path = '/Users/tatenda/Desktop/lab3/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+
 application = get_wsgi_application()
