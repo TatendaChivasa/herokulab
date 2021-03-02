564287b7af884ceca8335bc52eb6dfdc9f25a17c
TATENDA CHIVASA <tatenda@TATENDAs-MacBook-Pro.local>
2021-03-02 02:38:25 -0700

    questions

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -4,18 +4,13 @@ WSGI config for mysite project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-path = '/tatenda/Desktop/lab3/mysite'
-if path not in sys.path:
-    sys.path.append(path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
